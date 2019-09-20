99a20f57ffa5858e3435b18af4aa7ec9c5708b8b
Vesper <mac@VesperdeMacBook-Pro.local>
2019-09-20 21:03:12 +0800

    add header

---
+++ travel/src/pages/home/components/Header.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+        <img :src="url" class="icon">
+        返回</div>
+    <div class="header-input">
+        <img :src="url1" class="icon">
+        输入城市/景点/游玩主题</div>
+    <div class="header-right">城市
+        <img :src="url2" class="icon">
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HomeHeader',
+  data () {
+    return {
+      url: '/../../../static/images/fanhui.png',
+      url1: '/../../../static/images/fangdajing.png',
+      url2: '/../../../static/images/xiala.png'
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  .header
+    display: flex
+    line-height: .86rem
+    background: $bgColor
+    color: #fff
+    .header-left
+      width: .8rem
+      float: left
+      .icon
+        height: .25rem
+    .header-input
+      padding-left: .2rem
+      height: .64rem
+      line-height: .64rem
+      flex: 1
+      border-radius: .1rem
+      margin: .12rem 0 0 .2rem
+      background: #fff
+      color:#ccc
+      .icon
+        height: .4rem
+    .header-right
+      margin-left: .1rem
+      width: 1.24rem
+      float: right
+      .icon
+        height: .25rem
+</style>

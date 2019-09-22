0a87e0cf40e099e531f8354f19cd5d2614682b69
Vesper <you@example.com>
2019-09-22 14:29:14 +0800

    city

--- travel/src/pages/city/components/List.vue
+++ travel/src/pages/city/components/List.vue
@@ -12,86 +12,18 @@
       <div class="area">
         <div class="title">热门城市</div>
         <div class="button-list">
-          <div class="button-wrapper">
-            <div class="button">北京</div>
+          <div class="button-wrapper" v-for="item of hot" :key="item.id">
+            <div class="button">{{item.name}}</div>
           </div>
         </div>
       </div>
-      <div class="area">
-        <div class="title">A</div>
-        <div class="item-list">
+      <div class="area" v-for="(item,key) of cities" :key="key"> 
+        <div class="title">{{key}}</div>
+        <div class="item-list"
+        v-for="innerItem of item" :key="innerItem.id"
+        >
           <div class="item">
-            阿拉尔
-          </div>
-          <div class="item">
-            阿拉尔
-          </div>
-          <div class="item">
-            阿拉尔
-          </div>
-          <div class="item">
-            阿拉尔
-          </div>
-          <div class="item">
-            阿拉尔
-          </div>
-          <div class="item">
-            阿拉尔
-          </div>
-          <div class="item">
-            阿拉尔
-          </div>
-        </div>
-      </div>
-      <div class="area">
-        <div class="title">A</div>
-        <div class="item-list">
-          <div class="item">
-            阿拉尔
-          </div>
-          <div class="item">
-            阿拉尔
-          </div>
-          <div class="item">
-            阿拉尔
-          </div>
-          <div class="item">
-            阿拉尔
-          </div>
-          <div class="item">
-            阿拉尔
-          </div>
-          <div class="item">
-            阿拉尔
-          </div>
-          <div class="item">
-            阿拉尔
-          </div>
-        </div>
-      </div>
-      <div class="area">
-        <div class="title">A</div>
-        <div class="item-list">
-          <div class="item">
-            阿拉尔
-          </div>
-          <div class="item">
-            阿拉尔
-          </div>
-          <div class="item">
-            阿拉尔
-          </div>
-          <div class="item">
-            阿拉尔
-          </div>
-          <div class="item">
-            阿拉尔
-          </div>
-          <div class="item">
-            阿拉尔
-          </div>
-          <div class="item">
-            阿拉尔
+           {{innerItem.name}}
           </div>
         </div>
       </div>
@@ -102,6 +34,10 @@
 import BScroll from 'better-scroll'
 export default {
   name: 'CityList',
+  props: {
+    hot:Array,
+    cities:Object
+  },
   mounted () {
     this.scroll = new BScroll(this.$refs.wrapper)
   }
